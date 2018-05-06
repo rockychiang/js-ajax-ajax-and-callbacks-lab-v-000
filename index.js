afc2fb53fa7667780afb33c1875c0497186262dd
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-06 05:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function searchRepositories(){
   const url = 'https://api.github.com/search/repositories?q=' + searchTerms;
   
   $.get(url).done(data => {
-    const repoList = `<ul>${data.map(r => '<li><a href="'+ r.html_url +'">' + r.name + '</a> - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`;
+    const repoList = `<ul>${data.items.map(r => '<li><a href="'+ r.html_url +'">' + r.name + '</a> - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`;
     $("#results").html(repoList);
   });
 }
