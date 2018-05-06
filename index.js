27765ac316b063ff5a77996bb5597a042aa3ab99
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-06 06:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ function searchRepositories(){
   const url = `https://api.github.com/search/repositories?q=${searchTerms}`;
   
   $.get(url).done(data => {
-    const repoList = '<ul>' + data.map(r => {
-      return (`
-            <li>
-              <h2><a href="${r.html_url}">${r.name}</a></h2>
-              <p><img src="${r.owner.avatar_url}"></p>
-              <p>by: <a href="https://github.com/${r.owner.login}">${r.owner.login}</a></p>
-              <p><a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-            </li>`
-            );
-    }).join('') + "</ul>";
-    
+    const repoList = `
+      <li>
+        <h2><a href="${data.html_url}">${r.name}</a></h2>
+        <p><img src="${r.owner.avatar_url}"></p>
+        <p>by: <a href="https://github.com/${r.owner.login}">${r.owner.login}</a></p>
+        <p><a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      </li>`;
+            
     $("#results").html(repoList);
   }).fail(displayError);
 }
