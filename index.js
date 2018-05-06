80bdc58199f856a22863f0d702bde098ccef976c
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-06 04:43:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-  const searchTerms = .getElementById("searchTerms").value;
+  const searchTerms = document.getElementById("searchTerms").value;
   const url = 'https://api.github.com/search/repositories?q=' + document;
   
   $.get(url).done(data => )
