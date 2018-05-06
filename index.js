02104ad85875b8526e48b1bdd4b020a5f758f6f4
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-06 04:41:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ $(document).ready(function (){
   
 });
 
-function getRepositories() {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
-  req.send()
-}
-
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const src = document.getElementById("repository-template").innerHTML
